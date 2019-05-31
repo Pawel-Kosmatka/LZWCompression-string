c073fea45490d8c3eb60606eb27cf30bf543231c
Pawel Kosmatka <pawel.kosmatka@outlook.com>
2019-05-31 15:14:35 +0200

    result display implemented

--- LzwStringCompression/Program.cs
+++ LzwStringCompression/Program.cs
@@ -7,10 +7,25 @@ namespace LzwStringCompression
     {
         static void Main(string[] args)
         {
-            var compressed = Compress("Ala ma kota kot ma Ale");
+            var source = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl purus in mollis nunc. Faucibus nisl tincidunt eget nullam non nisi est. Ipsum suspendisse ultrices gravida dictum fusce ut. A cras semper auctor neque vitae tempus quam. Consectetur adipiscing elit ut aliquam purus. Bibendum enim facilisis gravida neque. Netus et malesuada fames ac turpis. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam. Quam elementum pulvinar etiam non quam lacus. Nisl tincidunt eget nullam non nisi est. Mauris in aliquam sem fringilla ut morbi tincidunt. Maecenas volutpat blandit aliquam etiam erat velit scelerisque. Aliquam eleifend mi in nulla posuere. Id aliquet risus feugiat in. Dictum non consectetur a erat nam at lectus urna. Sit amet commodo nulla facilisi. Vitae ultricies leo integer malesuada. Enim nec dui nunc mattis enim ut tellus. Sit amet volutpat consequat mauris nunc congue.";
+            var compressed = Compress(source);
+            Console.WriteLine("Compression result:");
             Console.WriteLine(string.Join(",", compressed));
+            Console.WriteLine("Decompressed string:");
             var decompressed = Decompress(compressed);
             Console.WriteLine(decompressed);
+
+            Console.WriteLine("Source string length:");
+            Console.WriteLine(source.Length);
+            Console.WriteLine("Compressed data length:");
+            Console.WriteLine(compressed.Count);
+            Console.WriteLine("Decompressed string length:");
+            Console.WriteLine(decompressed.Length);
+
+            Console.WriteLine("Compression ratio:");
+            Console.WriteLine(((double)source.Length / compressed.Count).ToString("F"));
+
+            Console.ReadLine();
         }
         public static List<int> Compress(string source)
         {
@@ -20,9 +35,9 @@ namespace LzwStringCompression
                 dictionary.Add(((char)i).ToString(), i);
             }
 
-            var c = string.Empty;
             var result = new List<int>();
 
+            var c = string.Empty;
             foreach (var s in source)
             {
                 string cs = c + s;
